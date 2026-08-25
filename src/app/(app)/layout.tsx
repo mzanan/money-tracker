7b031f2502ae33d2f9a9856ba779767578954a59
Matias Zanan <66926558+mzanan@users.noreply.github.com>
2026-08-25 21:23:27 +0700

    Revert "fix: align Drawer to content container edge on wide monitors (#54)" (#75)
    This reverts commit 424d7e533a03f656d20317eab339de91844c2515.

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -39,7 +39,7 @@ export default async function AppLayout({
       <AccountLabelsProvider value={accountLabels}>
       <HideAmountsProvider initial={hideAmounts}>
         <ConfirmProvider>
-        <div className="mx-auto flex w-full max-w-xl flex-1 flex-col lg:max-w-(--container-content-max)">
+        <div className="mx-auto flex w-full max-w-xl flex-1 flex-col lg:max-w-6xl">
           <Header />
           <main className="flex-1 px-4 pt-2 pb-24 lg:pb-8">{children}</main>
         </div>
